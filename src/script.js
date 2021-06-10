d2968670c34417531c7cf9ced187526c3c6a8ea1
Andres r hernandez <frodriguezh@unal.edu.co>
2021-06-10 16:33:36 -0500

    obligando al usuario a ingresar valores.

--- src/script.js
+++ src/script.js
@@ -4,7 +4,11 @@ const introButton = document.querySelector('.intro-button')
 const enterData = document.querySelector('.enter-data')
 const userDataName = document.querySelector('.form--userdata__name')
 const userDataBalance = document.querySelector('.form--userdata__balance-value')
-
+const transactionContainer = document.querySelector('.transaction-container')
+const buttonAdd = document.querySelector('.button-add')
+const transDescription = document.querySelector('.trans-description')
+const transAmmount = document.querySelector('.trans-ammount')
+const transType = document.querySelector('.trans-type')
 
 let user = {
   "name": String,
@@ -27,11 +31,28 @@ function getUserData () {
     userDataName.textContent = user.name
     userDataBalance.textContent = `${user.balance}`
     enterData.classList.add('hide')
+    transactionContainer.classList.remove('hide')
     console.log(user);
   }
 }
+
+function printTransaction () {
+  if(transAmmount.value === "") {
+    alert('Por favor ingresa una cantidad, para poder registrar tu transacción.')
+  } else if (transDescription.value === "") {
+    alert('Por favor agrega una descripción de tu transacción para continuar.')
+  } else {
+    alert('movimiento registrado')
+
+
+
+
+  }
+}
+
 
 
 introButton.addEventListener('click', getUserData)
 
 
+buttonAdd.addEventListener('click', printTransaction)
